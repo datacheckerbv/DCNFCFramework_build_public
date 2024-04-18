e6e92349163bfc88eb739acd88b9b6affadd7526
Omkar <omkar@datachecker.nl>
2024-04-18 18:07:14 +0530

    Updated code

---
+++ Sources/DCNFCFramework.xcframework/ios-arm64/DCNFCFramework.framework/Headers/DCNFCFramework.h
@@ -0,0 +1,18 @@
+//
+//  DCNFCFramework.h
+//  DCNFCFramework
+//
+//  Created by Omkar Awate on 21/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DCNFCFramework.
+FOUNDATION_EXPORT double DCNFCFrameworkVersionNumber;
+
+//! Project version string for DCNFCFramework.
+FOUNDATION_EXPORT const unsigned char DCNFCFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DCNFCFramework/PublicHeader.h>
+
+
